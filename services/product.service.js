3121d04121715ffe6a80167218f5addea34d42d2
Carlos Dugarte <soycarlosdugarte@gmail.com>
2024-10-19 17:59:20 -0300

    add and configured sequelize in project

--- services/product.service.js
+++ services/product.service.js
@@ -2,15 +2,13 @@ const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
 
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (error) => console.error(error))
   }
 
   generate() {
@@ -37,8 +35,10 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM task';
-    const rta = await this.pool.query(query)
-    return rta.rows;
+    const [ data ] = await sequelize.query(query)
+    return {
+      data,
+    };
   }
 
   async findOne(id) {
