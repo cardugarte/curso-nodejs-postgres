d54627fc3600ae64e97302888eb8d2b902bb55b7
Carlos Dugarte <soycarlosdugarte@gmail.com>
2024-10-19 16:34:01 -0300

    add pool conections in postgres

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,16 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+
+const pool = require('../libs/postgres.pool');
+
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (error) => console.error(error))
   }
 
   generate() {
@@ -30,8 +35,10 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM task';
+    const rta = await this.pool.query(query)
+    return rta.rows;
   }
 
   async findOne(id) {
